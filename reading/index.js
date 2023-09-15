f7c1862cb10878f05370eb2bc96f8a18b0ab2203
craigcliff <craigcliff@gmail.com>
2023-09-15 10:23:38 +0200

    Example of using transform and error listeners

--- reading/index.js
+++ reading/index.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 const csv = require("csvtojson");
+const { Transform } = require("stream");
 
 const main = async () => {
   const readStream = fs.createReadStream("./data/import.csv");
 
   const writeStream = fs.createWriteStream("./data/exports.csv");
 
-  //  We can use pipe to prevent backpressure
   readStream
     .pipe(
       csv(
@@ -19,8 +19,26 @@ const main = async () => {
         }
       )
     )
+    .pipe(
+      new Transform({
+        objectMode: true,
+        transform(chunk, enc, callback) {
+          console.log("chunk: >> ", chunk);
+
+          // callback method is required for stream to continue
+          callback("Some error");
+        },
+      })
+    )
     .on("data", (data) => {
       console.log("data: ", data);
+    })
+    .on("error", (error) => {
+      console.log("Stream error: ", error);
+    })
+    // Stream doesn't end if error is called
+    .on("end", () => {
+      console.log("Stream ended");
     });
 };
 
