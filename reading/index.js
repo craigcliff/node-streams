b2329b390a465562e36b3e3c56ee2883d2b3c9d7
craigcliff <craigcliff@gmail.com>
2023-09-15 10:15:58 +0200

    Using csvToJson and node built in objectMode to transform data to objects

--- reading/index.js
+++ reading/index.js
@@ -1,19 +1,27 @@
 const fs = require("fs");
+const csv = require("csvtojson");
 
 const main = async () => {
   const readStream = fs.createReadStream("./data/import.csv");
 
   const writeStream = fs.createWriteStream("./data/exports.csv");
 
-  readStream.pipe(writeStream);
-
-  readStream.on("end", () => {
-    console.log("Stream ended");
-  });
-
-  writeStream.on("finish", () => {
-    console.log("Write finised");
-  });
+  //  We can use pipe to prevent backpressure
+  readStream
+    .pipe(
+      csv(
+        {
+          delimiter: ";",
+        },
+        // built in node function that trasforms streams into objects
+        {
+          objectMode: true,
+        }
+      )
+    )
+    .on("data", (data) => {
+      console.log("data: ", data);
+    });
 };
 
 main();
