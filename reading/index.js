93febe9d29cc0e560e908d3e5806454e80f2c7a6
craigcliff <craigcliff@gmail.com>
2023-09-15 10:09:40 +0200

    Using pipe method on readStream to alleviate the possibility of backpressure

--- reading/index.js
+++ reading/index.js
@@ -1,24 +1,18 @@
 const fs = require("fs");
 
 const main = async () => {
-  const readStream = fs.createReadStream("./data/import.csv", {
-    // by default its 16kb, change it if you want chunk to be bigger or smaller
-    highWaterMark: 100,
-  });
+  const readStream = fs.createReadStream("./data/import.csv");
 
   const writeStream = fs.createWriteStream("./data/exports.csv");
 
-  readStream.on("data", (buffer) => {
-    console.log("data: ", buffer.toString());
-
-    // writeStream accepts a buffer or string
-    writeStream.write(buffer);
-  });
+  readStream.pipe(writeStream);
 
   readStream.on("end", () => {
     console.log("Stream ended");
+  });
 
-    writeStream.end();
+  writeStream.on("finish", () => {
+    console.log("Write finised");
   });
 };
 
